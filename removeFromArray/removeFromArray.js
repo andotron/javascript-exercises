4cbc2fa4f20b4d95bd3e718c0d7c9682f23c80e9
achen <38847889+andotron@users.noreply.github.com>
2020-10-15 19:11:25 -0700

    removeFromArray +implemented function +enabled and passed all tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...target) {
+    let arrCopy = arr; //copy array so original input is not touched
 
+    for (let i = 1; i < arguments.length;i++) {//iterate over args
+        if (arrCopy.indexOf(arguments[i]) !== -1) { //checks to see if target exists in arr
+            arrCopy.splice(arrCopy.indexOf(arguments[i]), 1); //deletes the target
+        }
+    }
+    return arrCopy;
 }
 
 module.exports = removeFromArray
