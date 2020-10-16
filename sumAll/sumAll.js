22d8cf472a7ca54f949a94825e3e4eff5ae65cd4
achen <38847889+andotron@users.noreply.github.com>
2020-10-15 19:20:27 -0700

    sumAll +implemented function +enabled and passed all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let sum = 0;
+    let large = start > stop ? start : stop;
+    let small = large === start ? stop : start;
 
+    if(start < 0 || stop < 0 || typeof start !== "number" || typeof stop !== "number") {
+        return 'ERROR';
+    }
+
+    for (let i = small; i <= large; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
