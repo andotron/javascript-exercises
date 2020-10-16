176b93beb72ca54e34eb58fa3e4bb91aa7324fe4
achen <38847889+andotron@users.noreply.github.com>
2020-10-15 18:35:46 -0700

    reverseString +implemented functional code +enabled and passed all tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strRev = ''; //hold final string
+    let strArr = string.split('');//split string to convert to array
+    for (let i = string.length - 1; i >= 0; i--) {//iterate from end of array(string.length) to first character.
+        strRev += strArr[i];//add each character to the stringbuilder
+    //return string builder
+    }
+    return strRev;
 }
 
 module.exports = reverseString
