cb193f2973f5379ed0c58fdeda34839ee9871b72
achen <38847889+andotron@users.noreply.github.com>
2020-10-16 17:22:22 -0700

    leapYears +implemented function +enabled and passed all tests

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) { //input is year
+    if (year % 100 === 0) { //leap years are divisable by 100 & 400
+        if (year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (year % 4 === 0) { //leap years are divisable by 4 
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = leapYears
